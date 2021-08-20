fd223f1816b8c78fc5be415bd2084d0cab6e6269
Rudy Lo <luhuadong@163.com>
2021-08-20 11:23:04 +0800

    add recipes-helloworld

---
+++ recipes-helloworld/helloworld-a/helloworld/src/helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <config.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, GetIoT.tech! (Build by Autotools)\n");
+    return 0;
+}
---
+++ recipes-helloworld/helloworld-m/helloworld/helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, GetIoT.tech!\n");
+    return 0;
+}
